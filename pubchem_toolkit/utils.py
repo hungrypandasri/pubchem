5efc39f5c0ebdcdd9acdae371bafb8cb3599de6b
unknown <hungrypandasriram@gmail.com>
2024-04-23 00:26:38 -0400

    Initial commit: Version 0.0.1

---
+++ pubchem_toolkit/utils.py
@@ -0,0 +1,36 @@
+# File: pubchem_toolkit/compound_search.py
+
+import requests
+
+def get_compound_properties(compound_name):
+    """
+    Retrieve compound properties from PubChem API based on compound name.
+
+    Args:
+        compound_name (str): Name of the compound.
+
+    Returns:
+        dict: Dictionary containing compound properties.
+    """
+    base_url = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{}/json"
+    url = base_url.format(compound_name)
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        properties = data['PC_Compounds'][0]['props']
+        compound_properties = {}
+        for prop in properties:
+            label = prop['urn']['label']
+            if 'sval' in prop['value']:
+                value = prop['value']['sval']
+            elif 'ival' in prop['value']:
+                value = prop['value']['ival']
+            elif 'fval' in prop['value']:
+                value = prop['value']['fval']
+            else:
+                value = None
+            compound_properties[label] = value
+        return compound_properties
+    else:
+        raise Exception("Failed to retrieve compound properties. Check compound name and try again.")
---
+++ tests/tests_utils.py
@@ -0,0 +1,18 @@
+# File: tests/test_compound_search.py
+
+import unittest
+from pubchem_toolkit.utils import get_compound_properties
+
+
+class TestCompoundSearch(unittest.TestCase):
+    def test_valid_compound(self):
+        properties = get_compound_properties("aspirin")
+        self.assertIsNotNone(properties)
+        self.assertIsInstance(properties, dict)
+
+    def test_invalid_compound(self):
+        with self.assertRaises(Exception):
+            get_compound_properties("nonexistentcompound")
+
+if __name__ == "__main__":
+    unittest.main()
